a78ffa0d412dfe9872018f1ff8de46632d750ef2
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-04-03 14:05:38 -0300

    update decorator valide auth

--- bottle_auth/decorator.py
+++ bottle_auth/decorator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from functools import wraps
+
 from bottle import redirect, request
 from bottle.ext import auth
-from bottle.ext.core.exception import UserDenied
 
 
 def login(url='/login'):
-    def wrapper(func):
+    def decorator(func):
         @wraps(func)
-        def check(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             try:
                 auth.get_user(request.environ)
-            except UserDenied:
-                redirect(url)
-            return func(*args, **kwargs)
-        return check
-    return wrapper
+                return func(*args, **kwargs)
+            except:
+                return redirect(url)
+        return wrapper
+    return decorator
