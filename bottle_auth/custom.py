7b1a349fc7e7173e82dbd807045fdf7d6516dfc1
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-04-03 17:54:36 -0300

    create auth custom used auth via native application

---
+++ bottle_auth/custom.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+from bottle import redirect
+
+
+log = logging.getLogger('bottle-auth.custom')
+
+
+class Custom(object):
+    def __init__(self, login_url="/login",
+                 callback_url="http://127.0.0.1:8000"):
+        self.login_url = login_url
+        self.callback_url = callback_url
+
+    def redirect(self, environ):
+        return redirect(self.login_url)
+
+    def get_user(self, environ):
+        session = environ.get('beaker.session')
+        if session.get("username", None) and session.get("apikey", None):
+            return session
+        return {}
