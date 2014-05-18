6e1d9179d3494a27e02ef861f181bd10383a7f3e
Thiago Avelino <thiago@avelino.xxx>
2014-05-18 10:42:56 -0300

    Set field name on custom auth
    used to check exits field on session

--- bottle_auth/custom.py
+++ bottle_auth/custom.py
@@ -4,20 +4,19 @@ import logging
 from bottle import redirect
 
 
-log = logging.getLogger('bottle-auth.custom')
-
-
 class Custom(object):
     def __init__(self, login_url="/login",
-                 callback_url="http://127.0.0.1:8000"):
+                 callback_url="http://127.0.0.1:8000",
+                 field="username"):
         self.login_url = login_url
         self.callback_url = callback_url
+        self.field = field
 
     def redirect(self, environ):
         return redirect(self.login_url)
 
     def get_user(self, environ):
         session = environ.get('beaker.session')
-        if len(session) >= 1:
+        if session.get(self.field):
             return session
         self.redirect(environ)
